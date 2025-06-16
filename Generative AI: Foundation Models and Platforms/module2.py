e51393ce637deacb35a7cc29e411926f0dc94246
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-16 14:41:26 -0500

    Create module2.py

---
+++ Generative AI: Foundation Models and Platforms/module2.py
@@ -0,0 +1,85 @@
+""""
+1.
+Question 1
+__________________ are able to generate a random image based on a text prompt and then add details to the image until it matches the text description.     
+
+
+Sequence-to-Sequence models  
+
+
+
+Diffusion models*****
+
+
+
+Generative adversarial networks 
+
+
+
+Transformer-based models  
+
+
+1 point
+2.
+Question 2
+Text-to-text generation models can be used successfully to _____________.   
+
+
+Generate programming scripts from natural language descriptions  
+
+
+
+Translate a poem into multiple languages *****
+
+
+
+Identify relationships between objects in an image 
+
+
+
+Forecast weather based on natural language descriptions 
+
+
+1 point
+3.
+Question 3
+Which one aspect of Hugging Face’s AI platform differentiates it from other machine learning platforms? It’s ability to ______________.   
+
+
+Engage the open-source AI community  ******
+
+
+
+Create innovative generative AI applications 
+
+
+
+Pursue the general “one model to rule” assumption 
+
+
+
+Leverage natural language processing capabilities 
+
+
+1 point
+4.
+Question 4
+Which of the following is the correct benefit of IBM watsonx.ai?
+
+
+ You can customize models based on your data and for your business use cases.  ****** 
+
+
+
+You can process data efficiently through multiple query engines.  
+
+
+
+You can view and access the models and data of other users.  
+
+
+
+You can analyze AI models and processes through dashboards, charts, and dimensional reporting. 
+
+
+1 point
