74227da9b350324002d9a763133719531490af96
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-16 14:35:39 -0500

    Create module1.py

---
+++ Generative AI: Foundation Models and Platforms/module1.py
@@ -0,0 +1,67 @@
+"""
+1.
+Question 1
+Noah wants to set up a deep learning framework to cluster similar documents. Which two critical components will he need to get started?
+
+An artificial neural network and labeled data.
+
+
+High number of parameters and an artificial neural network.
+
+
+Unlabeled data and a high number of parameters.
+
+
+An artificial neural network and unlabeled data.****
+
+1 point
+2.
+Question 2
+Katya is looking for a diffusion model that can help her generate high-quality graphical content. Which one do you recommend?
+
+Bidirectional Encoder Representations from Transformers (BERT)
+
+
+OpenAI’s GPT-4
+
+
+OpenAI’s Dall-E 2****
+
+
+SpaceGAN
+
+1 point
+3.
+Question 3
+_________________ use two convolutional neural networks to compete against each other to produce more realistic data samples.
+
+Diffusion models
+
+
+Variational autoencoders
+
+
+Transformer-based models
+
+
+Generative adversarial networks****
+
+1 point
+4.
+Question 4
+Foundation generative AI models are distinct from other generative AI models because they _______________.
+
+Is trained on restricted domain data
+
+
+Perform only image classification tasks
+
+
+Provides a predetermined response to queries
+
+
+Exhibit broad capabilities that can be adapted to a range of different and specific tasks*****
+
+1 point
+
+
