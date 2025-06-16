3ecfbe3278a7fa739cbcd0107dbd6cc0c2e6ac5c
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-16 14:44:20 -0500

    Create module3.py

---
+++ Generative AI: Foundation Models and Platforms/module3.py
@@ -0,0 +1,148 @@
+""""
+1.
+Question 1
+Lina is part of a group of developers tasked with creating a sample large language model (LLM). Which deep generative network should they use to accomplish this task? 
+
+
+Supervised learning algorithms  
+
+
+
+A convolutional neural network
+
+
+
+A transformer-based model ****
+
+
+
+A recurrent neural network 
+
+
+1 point
+2.
+Question 2
+While working with a recurrent neural network, Nitin struggles to process long sequences of text. Which type of generative AI model can he use to model long-term dependencies in text? 
+
+
+Diffusion models 
+
+
+
+Variational autoencoders 
+
+
+
+Generative adversarial networks
+
+
+
+Transformer-based models ******
+
+
+1 point
+3.
+Question 3
+Huma is searching the internet for information on the twelve ways to stay alert on night watch. Which foundation model will likely give her the best result for such a query? 
+
+
+Meta’s Galactica
+
+
+
+OpenAI’s GPT-4  *****
+
+
+
+AlexNet
+
+
+
+OpenAI’s DALL-E
+
+
+1 point
+4.
+Question 4
+Neil is developing an application that lets users type in text descriptions to create unique images. Which generative AI model will be most suitable for implementing this feature?  
+
+
+A generative adversarial network *****
+
+
+
+A variational autoencoder 
+
+
+
+A Markov chain
+
+
+
+T5 
+
+
+1 point
+5.
+Question 5
+Anita from the IT department is trying to develop an application that can automatically generate code from a text prompt. Which generative AI model will be most suitable for implementing this feature? 
+
+
+A variational autoencoder 
+
+
+
+Style-generative adversarial network (StyleGAN)
+
+
+
+Bidirectional Encoder Representations from Transformers (BERT) 
+
+
+
+PanGu-Coder *****
+
+
+1 point
+6.
+Question 6
+Veronique has just signed onto the IBM watsonx.ai platform. Which of these tasks can she perform on this platform? 
+
+
+Use dashboards to analyze other AI models on the platform 
+
+
+
+Customize models based on her company’s business use cases *****
+
+
+
+Use multiple query engines to process data efficiently  
+
+
+
+Perform dimensional reporting visibility into your AI models 
+
+
+1 point
+7.
+Question 7
+Chandra is searching the Hugging Face website for a tool to help her develop her transformer-based model. Which Hugging Face tool is most likely to help her do that?
+
+
+Spaces tab
+
+
+
+Transformers library*****
+
+
+
+HuggingChat 
+
+
+
+Expert Acceleration Program 
+
+
+1 point
