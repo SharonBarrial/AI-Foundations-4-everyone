12f8af065e0267592d6d996b0d5f7c3e47f60486
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-16 14:59:18 -0500

    Create module1.py

---
+++ Generative AI: Impact, Considerations, and Ethical Issues/module1.py
@@ -0,0 +1,85 @@
+""""
+1.
+Question 1
+How does overfitting contribute to hallucination in text and image-generating LLMs?
+
+
+ Overfitting ensures inaccurate generation by LLMs.  
+
+
+
+Overfitting leads to a limited understanding of training data.****
+
+
+
+Overfitting prevents the generation of novel content. 
+
+
+
+Overfitting increases the complexity of LLMs.
+
+
+1 point
+2.
+Question 2
+What is the potential legal and ethical question that might arise if a gen AI model hallucinates code for a medical device, leading to incorrect treatments?
+
+
+Questions about the speed and automation benefits of LLMs
+
+
+
+Questions about the responsibility of developers and creators of the AI model ******
+
+
+
+Questions about the syntax and semantics of programming language used in medical devices
+
+
+
+Questions about the effectiveness of error-handling mechanisms in LLMs
+
+
+1 point
+3.
+Question 3
+Why does the lack of creative control become a challenge in AI-assisted portraits?
+
+
+Difficulty in creating diverse datasets for gen AI models
+
+
+
+Results are based on the pattern established by machine learning algorithms*******
+
+
+
+Insufficient power of computing displayed by generative AI models
+
+
+
+Lack of skilled artists using the gen AI technology
+
+
+1 point
+4.
+Question 4
+Which among the following raises concerns about accountability and reliability and is identified as a significant limitation of generative AI models from the users' perspective?
+
+
+Difficulty in obtaining sufficient data for training
+
+
+
+High cost, computational resources, and training time
+
+
+
+Inability to generate content based on current information and events
+
+
+
+Lack of transparency and predictability **********
+
+
+1 point
