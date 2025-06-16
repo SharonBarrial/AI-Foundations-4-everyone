6decdb9ede74619930516049c9262f4eb899b4ab
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-16 14:13:42 -0500

    Create module2.py

---
+++ Generative AI: Prompt Engineering Basics/module2.py
@@ -0,0 +1,87 @@
+"""
+1.
+Question 1
+Imagine you are planning a business trip, and you want to use the interview pattern approach to prompt an AI model to assist you in planning your itinerary. What would be the benefit of this approach in comparison to a traditional static prompt?
+
+
+The model will minimize the need for any user interaction
+
+
+
+The model will generate a random travel plan.
+
+
+
+The model will ask for your preferences and adjust the itinerary accordingly****
+
+
+
+The model will provide a single predetermined itinerary
+
+
+1 point
+2.
+Question 2
+Jennifer wants to request some useful information about a complex medical condition using a large language model. Which among the following techniques should she employ on the text prompt to ensure that the generated content is relevant, accurate, and precise for this specialized field?
+
+
+Bias mitigation
+
+
+
+Framing
+
+
+
+Domain expertise****
+
+
+
+Task specification
+
+
+1 point
+3.
+Question 3
+Imagine you are a content developer working with LLMs, and you must ensure that the responses generated are indiscriminatory and neutral. Which among the following techniques would you employ with your text prompts to instruct the model appropriately?
+
+
+Contextual guidance
+
+
+
+Few-shot prompting
+
+
+
+Zero-shot prompting
+
+
+
+Bias mitigation ****
+
+
+1 point
+4.
+Question 4
+Imagine you are using the Chain-of-Thought approach to teach a generative AI model how to solve a mathematical problem. What is the key component of a prompt in this approach?
+
+
+A prompt consists of a question and a correct answer for context.****
+
+
+
+A prompt includes a series of related questions without the correct answer.
+
+
+
+A prompt includes only a question without an answer.
+
+
+
+A prompt includes a list of formulae to solve the question.
+
+
+1 point
+
+"""
