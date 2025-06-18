f3b2d970b8eb6aebe3673f9f3324375e84ef338b
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-18 01:32:45 -0500

    Create module2.py

---
+++ Generative AI: Impact, Considerations, and Ethical Issues/module2.py
@@ -0,0 +1,19 @@
+""""
+Question 1
+In her report titled ‘Generative AI Insights’, Anita has identified ________________ as the business function that is expected to see the highest growth, but also cause the maximum job displacement due to generative AI.
+✅ Customer operations
+
+Question 2
+Charlie is interested in working in the field of generative AI. His colleague informs him that the job role of a __________________ has gained prominence due to the increasing use of foundation models.
+✅ Prompt engineer
+
+Question 3
+As the vice president of a big AI firm, what action can Amar take to reduce the firm’s carbon footprint?
+✅ Fine-tuning existing foundation models for downstream tasks
+(This reduces the need to train models from scratch, which is resource-intensive.)
+
+Question 4
+As the lead developer in her organization, Tina is tasked with ensuring that their large language model (LLM) protects the privacy of user data. How can she accomplish this?
+✅ Train the LLM using privacy-preserving algorithms
+
+"""
