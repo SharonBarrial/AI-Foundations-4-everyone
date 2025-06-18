1a6b8fc3ed6f84f80a38f5f73b1869b94fd6ae25
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-18 02:20:47 -0500

    Create module2.py

---
+++ Generative AI: Business Transformation and Career Growth/module2.py
@@ -0,0 +1,11 @@
+""""
+
+Gain deeper insights into customer behavior
+
+Design and build large language models to respond to human queries
+
+Marketing
+
+Guarantee reliability and security of automated processes
+
+"""""
