73b72c20e0a8a7cb8e83bf2834e55017033fcced
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-18 01:36:45 -0500

    Create module3.py

---
+++ Generative AI: Impact, Considerations, and Ethical Issues/module3.py
@@ -0,0 +1,30 @@
+""""
+Question 1
+Despite easy access to generative AI tools, many people across the world will not be able to leverage them because of ____________.
+✅ Digital exclusion
+
+Question 2
+To ensure ______________ and ethical decision-making, companies can offer users non-technical explanations regarding the limits, capabilities, and risks associated with generative AI.
+✅ Transparency
+
+Question 3
+How can you improve the monitoring and reporting function to increase the safety associated with a generative AI model?
+✅ Continuously Oversee AI model behavior and provide issue reporting and resolution mechanisms.
+
+Question 4
+________________ is one of the most significant limitations generative AI faces today.
+✅ Inability to generate new knowledge beyond training data
+
+Question 5
+What is a possible outcome resulting from inadequately sampled training data in generative AI models?
+✅ Introduction of inaccuracies and biases
+
+Question 6
+How can deepfake videos produced using generative AI models pose a threat to financial security and privacy?
+✅ Creating forged identity documents for fraudulent purposes
+
+Question 7
+Which of the following options accurately explains the meaning of the term "hallucinations" in code-generating large language models (LLMs)?
+✅ Nonsensical or irrelevant code generation
+
+"""
