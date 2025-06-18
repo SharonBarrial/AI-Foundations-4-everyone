4eb9971da29e24f4ea8182e9f186694389f67933
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-18 02:05:22 -0500

    Create module1.py

---
+++ Generative AI: Business Transformation and Career Growth/module1.py
@@ -0,0 +1,13 @@
+"""
+Banking Industry
+
+
+Augmentation in performance of real-time applications
+
+
+To assess workforce requirements and model impact
+
+
+
+✅ Improve multimodal capabilities of foundation models
+
